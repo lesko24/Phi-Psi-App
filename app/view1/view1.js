c7d1d89c0ffa589c1fc337bce29bb0ea3d442fa5
Jess Karol <jpk0727@gmail.com>
2015-08-19 00:24:41 -0400

    added login functionality with fire

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var phiapp = angular.module('myApp.view1', ['ui.router'])
+var phiapp = angular.module('myApp.view1', ['ui.router','firebase'])
 
 phiapp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
@@ -21,6 +21,16 @@ phiapp.config(function($stateProvider, $urlRouterProvider) {
       url:'/alumni',
       templateUrl: 'view1/alumni.html',
       controller: 'AlumniCtrl'
+    })
+    .state('login', {
+      url:'/login',
+      templateUrl: 'view1/login.html',
+      controller: 'LoginCtrl'
+    })
+    .state('register', {
+      url:'/register',
+      templateUrl: 'view1/register.html',
+      controller: 'RegisterCtrl'
     });
 })
 
@@ -35,3 +45,53 @@ phiapp.controller('AboutCtrl', function($scope) {
 phiapp.controller('AlumniCtrl', function($scope) {
   $scope.message = 'Look how good I look!';
 });
+
+phiapp.controller('LoginCtrl',['$scope','$rootScope','$firebaseAuth','$location',
+		function($scope,$rootScope,$firebaseAuth,$location) {
+
+	var ref = new Firebase("https://popping-inferno-3537.firebaseio.com");
+	var auth = $firebaseAuth(ref);
+	
+	$scope.user = {};
+	$scope.SignIn = function(e) {
+		ref.authWithPassword({
+			email : $scope.user.email,
+			password : $scope.user.password
+		}, function(error, authData) {
+			if (error) {
+				console.log('Authentication faulure');
+			} else {
+				console.log('Authentication Successfull');
+				$location.path('/home');
+			}
+		});
+	}
+
+}]);
+
+phiapp.controller('RegisterCtrl', ['$scope','$location','$firebaseAuth', function($scope,$location,$firebaseAuth) {
+	var firebaseObj = new Firebase("https://popping-inferno-3537.firebaseio.com");
+	var auth = $firebaseAuth(firebaseObj);
+	$scope.signUp = function() {
+		if (!$scope.regForm.$invalid) {
+			var email = $scope.user.email;
+			var password = $scope.user.password;
+			if (email && password) {
+				auth.$createUser({
+					email : email,
+					password : password
+				})
+					.then(function() {
+						// do things if success
+						console.log('User creation success');
+						$location.path('/home');
+					}, function(error) {
+						// do things if failure
+						console.log(error);
+						$scope.regError = true;
+						$scope.regErrorMessage = error.message;
+					});
+			}
+		}
+	};
+}]);
